7ffa8f01604ddf9a9b8b45d859c418c5b1a69ba4
Thomas Jouaville <t.jouaville@gmail.com>
2022-08-11 22:46:20 +0200

    Create login and register page

---
+++ app/components/form-field.tsx
@@ -0,0 +1,33 @@
+interface FormFieldProps {
+  htmlFor: string
+  label: string
+  type?: string
+  value: any
+  onChange?: (...args: any) => any
+}
+
+const FormField: React.FC<FormFieldProps> = ({
+  htmlFor,
+  label,
+  type = 'text',
+  value,
+  onChange = () => {},
+}) => {
+  return (
+    <>
+      <label htmlFor={htmlFor} className='text-blue-600 font-semibold'>
+        {label}
+      </label>
+      <input
+        onChange={onChange}
+        type={type}
+        id={htmlFor}
+        name={htmlFor}
+        className='w-full p-2 rounded-xl my-2'
+        value={value}
+      />
+    </>
+  )
+}
+
+export default FormField
---
+++ app/components/layout.tsx
@@ -0,0 +1,5 @@
+const Layout: React.FC = ({ children }) => {
+  return <div className='h-screen w-full bg-blue-600 font-mono'>{children}</div>
+}
+
+export default Layout
