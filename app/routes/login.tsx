ae07d8ab04b17355018246a25b18c1ad7d127774
Thomas Jouaville <t.jouaville@gmail.com>
2022-08-18 19:11:45 +0200

    Implement signin/signup forms + login process

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,7 +1,78 @@
 import { useState } from 'react'
+import { ActionFunction, json, LoaderFunction, redirect } from '@remix-run/node'
+
+import {
+  validateEmail,
+  validatePassword,
+  validateName,
+} from '~/utils/validators.server'
+import { getUser, login, register } from '~/utils/auth.server'
 
 import Layout from '~/components/layout'
 import FormField from '~/components/form-field'
+
+export const loader: LoaderFunction = async ({ request }) => {
+  // If there's already a user in the session, redirect to the home page
+  return (await getUser(request)) ? redirect('/') : null
+}
+
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData()
+  const action = form.get('_action')
+  const email = form.get('email')
+  const password = form.get('password')
+  let firstName = form.get('firstName')
+  let lastName = form.get('lastName')
+
+  if (
+    typeof action !== 'string' ||
+    typeof email !== 'string' ||
+    typeof password !== 'string'
+  ) {
+    return json({ error: `Invalid Form Data`, form: action }, { status: 400 })
+  }
+
+  if (
+    action === 'register' &&
+    (typeof firstName !== 'string' || typeof lastName !== 'string')
+  ) {
+    return json({ error: `Invalid Form Data`, form: action }, { status: 400 })
+  }
+
+  const errors = {
+    email: validateEmail(email),
+    password: validatePassword(password),
+    ...(action === 'register'
+      ? {
+          firstName: validateName((firstName as string) || ''),
+          lastName: validateName((lastName as string) || ''),
+        }
+      : {}),
+  }
+
+  if (Object.values(errors).some(Boolean))
+    return json(
+      {
+        errors,
+        fields: { email, password, firstName, lastName },
+        form: action,
+      },
+      { status: 400 }
+    )
+
+  switch (action) {
+    case 'login': {
+      return await login({ email, password })
+    }
+    case 'register': {
+      firstName = firstName as string
+      lastName = lastName as string
+      return await register({ email, password, firstName, lastName })
+    }
+    default:
+      return json({ error: `Invalid Form Data` }, { status: 400 })
+  }
+}
 
 const Login = () => {
   const [action, setAction] = useState('login')
@@ -63,7 +134,6 @@ const Login = () => {
                 onChange={(e) => handleInputChange(e, 'firstName')}
                 value={formData.firstName}
               />
-
               <FormField
                 htmlFor='lastName'
                 label='Last Name'
